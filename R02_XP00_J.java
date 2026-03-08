aed7cc545674d080eb469eb765022b6734c76a8a
al497700 <al497700@ucf.edu>
2026-03-08 11:38:40 -0400

    Update R02_XP00_J.java
    compliant version

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -1,9 +1,10 @@
 // Rule 02. Expressions (EXP) Example 0
-// NonCompliant
+// Compliant
 
 public void deleteFile() {
     File someFile = new File("someFileName.txt");
     // Do something with someFile
-    someFile.delete();
-
+    if (!someFile.delete()) {
+        // Handle failure to delete the file
+    }
 }
