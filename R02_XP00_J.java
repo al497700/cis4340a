8439b229c71d4590d5184e528b07b8596ed75234
al497700 <al497700@ucf.edu>
2026-03-08 11:28:04 -0400

    Update R02_XP00_J.java
    adding comments

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -1,5 +1,9 @@
+// Rule 02. Expressions (EXP) Example 0
+// NonCompliant
+
 public void deleteFile() {
     File someFile = new File("someFileName.txt");
     // Do something with someFile
     someFile.delete();
+
 }
