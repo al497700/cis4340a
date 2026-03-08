b13547b60e676e2cf640bf027c187c708b2ef66a
al497700 <al497700@ucf.edu>
2026-03-08 11:36:32 -0400

    Update R00_IDS03_J.java
    compliant version

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,10 +1,14 @@
 // Rule 00. Input Validation and Data Sanitization (IDS) Example 3
-// NonCompliant
+// Compliant
 
 if (loginSuccessful) {
-    logger.severe("User login succeeded for: " + username);
+    logger.severe("User login succeeded for: " + sanitizeUser(username));
 } else {
-    logger.severe("User login failed for: " + username);
+    logger.severe("User login failed for: " + sanitizeUser(username));
+}
 
+public String sanitizeUser(String username) {
+    return (Pattern.matches("[A-Za-z0-9]+", username))
+        ? username : "unauthorized user";
 }
 
