54033ba2f43e451c4d60372067a52c73f9d219fb
al497700 <al497700@ucf.edu>
2026-03-08 11:42:59 -0400

    Update R07_ERR08_J.java
    compliant version

--- R07_ERR08_J.java
+++ R07_ERR08_J.java
@@ -1,15 +1,14 @@
 // Rule 07. Exceptional Behavior (ERR) Example 8
-// NonCompliant
+// Compliant
 
 boolean isName(String s) {
-  try {
-    String names[] = s.split(" ");
-    if (names.length != 2) {
-      return false;
-    }
-    return (isCapitalized(names[0]) && isCapitalized(names[1]));
-  } catch (NullPointerException e) {
+  if (s == null) {
     return false;
   }
-
+  String names[] = s.split(" ");
+  if (names.length != 2) {
+    return false;
+  }
+  return (isCapitalized(names[0]) && isCapitalized(names[1]));
 }
+
