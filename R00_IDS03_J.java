848a7da6626c71ac64ecc1d777e13d2731f63826
al497700 <al497700@ucf.edu>
2026-03-08 11:25:31 -0400

    Update R00_IDS03_J.java
    added comments

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,5 +1,8 @@
+// Rule 00. Input Validation and Data Sanitization (IDS)
+
 if (loginSuccessful) {
     logger.severe("User login succeeded for: " + username);
 } else {
     logger.severe("User login failed for: " + username);
+
 }
